73e663fa7fb23b28b9b767c00cb7a18f29c2a708
Alex Ekstrom <aekstrom@flexport.com>
2021-12-02 10:55:57 -0600

    Some simplifications

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,32 +1,30 @@
-fn parse(data: &str) -> Vec<(u32, String)> {
-    return data
-        .lines()
-        .map(|i| {
-            let mut spl = i.split(' ');
-            let inst = spl.next().unwrap().to_string();
-            return (spl.next().unwrap().parse::<u32>().unwrap(), inst);
-        })
-        .collect();
+use itertools::Itertools;
+
+fn parse(data: &str) -> Vec<(u32, &str)> {
+    data.split_whitespace()
+        .tuples()
+        .map(|(inst, x)| (x.parse().unwrap(), inst))
+        .collect::<Vec<_>>()
 }
 
 fn solve_a(data: &[(u32, String)]) -> u32 {
-    let dist = data.into_iter().fold((0, 0), |a, b| match b.1.as_str() {
+    let (x, y) = data.into_iter().fold((0, 0), |a, b| match b.1.as_str() {
         "forward" => (a.0 + b.0, a.1),
         "down" => (a.0, a.1 + b.0),
         "up" => (a.0, a.1 - b.0),
         _ => a,
     });
-    return dist.0 * dist.1;
+    x * y
 }
 
 fn solve_b(data: &[(u32, String)]) -> u32 {
-    let dist = data.into_iter().fold((0, 0, 0), |a, b| match b.1.as_str() {
+    let (x, y, _) = data.into_iter().fold((0, 0, 0), |a, b| match b.1.as_str() {
         "forward" => (a.0 + b.0, a.1 + (b.0 * a.2), a.2),
         "down" => (a.0, a.1, a.2 + b.0),
         "up" => (a.0, a.1, a.2 - b.0),
         _ => a,
     });
-    return dist.0 * dist.1;
+    x * y
 }
 
 aoc2021::main!(2);
